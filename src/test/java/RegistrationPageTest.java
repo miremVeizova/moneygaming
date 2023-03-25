c59a2642d5e1697f2290aeacc1f88a98996e5da8
Miyrem Veizova <mirem.veizova@gmail.com>
2023-03-25 20:18:44 +0200

    CI setup

--- src/test/java/RegistrationPageTest.java
+++ src/test/java/RegistrationPageTest.java
@@ -1,19 +1,17 @@
 
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.engine.Constants;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class RegistrationPage {
+public class RegistrationPageTest {
 
     private WebDriver driver;
 
