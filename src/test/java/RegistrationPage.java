97cd04d8f38b612469a4ce6063ed184039b84e0c
Miyrem Veizova <mirem.veizova@gmail.com>
2023-03-25 12:31:01 +0200

    add variables change xpath for error message

--- src/test/java/RegistrationPage.java
+++ src/test/java/RegistrationPage.java
@@ -1,31 +1,30 @@
-import dev.failsafe.internal.util.Assert;
-import org.apache.commons.lang3.RandomStringUtils;
+
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-import java.time.Instant;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RegistrationPage {
 
+
+    public String loginButton = "//*[@class='login_links']//*[@class='newUser green']";
+
     private WebDriver driver;
-     WebDriverWait wait;
+    WebDriverWait wait;
 
     @BeforeEach
     public void beforeEachTest() {
-        driver = new FirefoxDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-        driver.get("https://moneygaming.qa.gameaccount.com/");
 
     }
 
@@ -35,50 +34,59 @@ public class RegistrationPage {
     }
 
 
-   @Test
+    @Test
     @DisplayName("Register a new user")
-    public void registerANewUser(){
+    public void registerANewUser() {
 
+
+        driver.get("https://moneygaming.qa.gameaccount.com/");
+        // wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//       WebElement loginHeading = driver.findElement(By.xpath("/html/head"));
+//       wait.until(ExpectedConditions.visibilityOf(loginHeading));
+
+
+//        var loginButton = "//*[@class='login_links']//*[@class='newUser green']";
 //       //Check that the navigation was successful
 //       WebElement loginHeading = driver.findElement(By.xpath("//*[@class='header']//*[@title='MoneyGaming']"));
 //       Assertions.assertEquals("MoneyGaming", loginHeading.getText());
 
+
         //Click the JOIN NOW button to open the registration page
-       WebElement joinNow = driver.findElement(By.xpath("//*[@class='login_links']//*[@class='newUser green']"));
-       joinNow.click();
-       WebElement titleField = driver.findElement(By.id("title"));
-       titleField.click();
+        WebElement joinNow = driver.findElement(By.xpath(loginButton));
+        joinNow.click();
+
+
+        WebElement titleField = driver.findElement(By.id("title"));
+        titleField.click();
 
 //       Select a title value from the dropdown
-       WebElement titleSelect = driver.findElement(By.name("map(title)"));
-       Select titleDropDown = new Select(titleSelect);
-       titleDropDown.selectByValue("Mrs");
+        WebElement titleSelect = driver.findElement(By.name("map(title)"));
+        Select titleDropDown = new Select(titleSelect);
+        titleDropDown.selectByValue("Mrs");
 
 //       Enter your first name
-       WebElement firstname = driver.findElement(By.id("forename"));
-       firstname.sendKeys("TestMiyrem");
+        WebElement firstname = driver.findElement(By.id("forename"));
+        firstname.sendKeys("TestMiyrem");
 
 //       Enter your last name
-       WebElement lastname = driver.findElement(By.name("map(lastName)"));
-       lastname.sendKeys("TestVeizova");
+        WebElement lastname = driver.findElement(By.name("map(lastName)"));
+        lastname.sendKeys("TestVeizova");
 
 //       Check the tickbox with text 'I accept the Terms and Conditions and certify that I am over the age of 18.'
-       WebElement tickbox = driver.findElement(By.xpath("//input[@name='map(terms)']"));
-           if(!tickbox.isSelected()){
+        WebElement tickbox = driver.findElement(By.xpath("//input[@name='map(terms)']"));
+        if (!tickbox.isSelected()) {
             tickbox.click();
-        } else{
+        } else {
             System.out.println(" Terms and Conditions button is already selected");
         }
-       assertTrue(tickbox.isSelected());
+        assertTrue(tickbox.isSelected());
 
 //Submit the form by clicking the JOIN NOW button
-       driver.findElement(By.xpath("//input[@id='form']")).click();
+        driver.findElement(By.xpath("//input[@id='form']")).click();
 
 //       Validate that a validation message with text ‘This field is required’ appears under the date of birth box
-       WebElement errorMessage = driver.findElement(By.xpath("//*[@class='dobDay inline required error']"));
-       Assertions.assertEquals("This field is required", errorMessage.getText());
-
-
+        WebElement errorMessage = driver.findElement(By.xpath("//*[@id='signupForm']/fieldset[1]/label[5]"));
+        Assertions.assertEquals("This field is required", errorMessage.getText());
 
 
 //       assertTrue(driver.getPageSource().contains("Your order has been placed!"));
@@ -110,6 +118,6 @@ public class RegistrationPage {
 //       String emailAddress = prefix + "@" + domainPrefix + "." + mainDomain;
 //       driver.findElement(By.name("map(email)")).sendKeys(emailAddress);
 
-   }
-   }
+    }
+}
 
